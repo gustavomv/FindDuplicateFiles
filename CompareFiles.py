455b263caa6c7657f4a2b09722e6b23981e78e94
Gustavo Moraes Vasconcelos <gmv_facul@hotmail.com>
2019-04-24 05:39:43 -0300

    Add files via upload

--- CompareFiles.py
+++ CompareFiles.py
@@ -6,24 +6,24 @@ duplicates = {}
 
 def CompFiles(FileDicData):
 
-	for file in FileDicData:
-		for path1, path2 in itertools.combinations(FileDicData[file], 2):
+	for fileName in FileDicData:
+		for path1, path2 in itertools.combinations(FileDicData[fileName], 2):
 			file1Size = os.stat(path1).st_size
 			file2Size = os.stat(path2).st_size
 
 			if (file1Size == file2Size):
 				file1Hash = hashfile(path1)
 				file2Hash = hashfile(path2)
-				
+				#"KEY = HASH+FILENAME"
 				if(file1Hash == file2Hash):
-					if (file1Hash in duplicates):
-						if (path1 not in duplicates[file1Hash]):
-							duplicates[file1Hash].append(path1)
-						if (path2 not in duplicates[file2Hash]):
-							duplicates[file2Hash].append(path2)
+					if ((file1Hash+fileName) in duplicates):
+						if (path1 not in duplicates[file1Hash+fileName]):
+							duplicates[file1Hash+fileName].append(path1)
+						if (path2 not in duplicates[file2Hash+fileName]):
+							duplicates[file2Hash+fileName].append(path2)
 					else:
-						duplicates[file1Hash] = [path1]
-						duplicates[file2Hash].append(path2)
+						duplicates[file1Hash+fileName] = [path1]
+						duplicates[file2Hash+fileName].append(path2)
 					print("find duplicate! writing output...")
 	print('\n')
 	return duplicates
